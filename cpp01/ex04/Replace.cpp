3f89dc26c003f3ae323a1d4dfe9e44bef7760558
CLMartello <carolinamartello1@gmail.com>
2026-05-01 19:48:30 +0100

    breaking cpp01 ex04 in dif functions

---
+++ cpp01/ex04/Replace.cpp
@@ -0,0 +1,29 @@
+
+#include "Replace.hpp"
+
+int    replace(std::string filename, std::string s1, std::string s2) {
+    
+    if (filename.empty() || s1.empty() || s2.empty())
+        return (std::cerr << "Arguments can not be empty\n", 1);
+    
+    std::ifstream file(filename);
+    if (!file.is_open())
+        return (std::cerr << "Error in file opening\n", 1);
+    
+    std::string line;
+    std::ofstream output(filename + ".replace");
+    
+    while (std::getline(file, line)) {
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos) {
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+            pos += s2.length();
+        }
+        output << line << "\n";
+    }
+    
+    file.close();
+    output.close();
+    return (0);
+}
--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,33 +1,14 @@
 
-#include <iostream> //input and output handling
-#include <string>
-#include <fstream> //file handling
-#include <cstdlib> //exit function
-
+#include "Replace.hpp"
 
 int main(int argc, char **argv) {
+
     if (argc != 4)
         return (std::cerr << "Arguments must be: ./program filename s1 s2\n", 1);
+    
     std::string filename = argv[1];
     std::string s1 = argv[2];
     std::string s2 = argv[3];
-    if (filename.empty() || s1.empty() || s2.empty())
-        return (std::cerr << "Arguments can not be empty\n", 1);
-    std::ifstream file(filename);
-    if (!file.is_open())
-        return (std::cerr << "Error in file opening\n", 1);
-    std::string line;
-    std::ofstream output(filename + ".replace");
-    while (std::getline(file, line)) {
-        size_t pos = 0;
-        while ((pos = line.find(s1, pos)) != std::string::npos) {
-            line.erase(pos, s1.length());
-            line.insert(pos, s2);
-            pos += s2.length();
-        }
-        output << line << "\n";
-    }
-    file.close();
-    output.close();
-    return (0);
+
+    return (replace(filename, s1, s2));
 }
