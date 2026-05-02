dda4c132c4ba5c3a676bd680cdd0d5ef691ca45c
CLMartello <carolinamartello1@gmail.com>
2026-05-02 21:40:29 +0100

    fix cpp01 ex04 main

--- cpp01/ex04/Replace.cpp
+++ cpp01/ex04/Replace.cpp
@@ -6,12 +6,14 @@ int    replace(std::string filename, std::string s1, std::string s2) {
     if (filename.empty() || s1.empty() || s2.empty())
         return (std::cerr << "Arguments can not be empty\n", 1);
     
-    std::ifstream file(filename);
+    std::ifstream file;
+    file.open(filename.c_str());
     if (!file.is_open())
         return (std::cerr << "Error in file opening\n", 1);
     
     std::string line;
-    std::ofstream output(filename + ".replace");
+    std::ofstream output;
+    output.open((filename + ".replace").c_str());
     
     while (std::getline(file, line)) {
         size_t pos = 0;
