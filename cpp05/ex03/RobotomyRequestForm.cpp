d440dd28dc671e6d9ffa0c548508de0541413963
CLMartello <carolinamartello1@gmail.com>
2026-06-04 19:59:37 +0100

    fix typo and random function

--- cpp05/ex03/RobotomyRequestForm.cpp
+++ cpp05/ex03/RobotomyRequestForm.cpp
@@ -3,6 +3,7 @@
 #include "AForm.hpp"
 #include "Bureaucrat.hpp"
 #include <cstdlib>
+#include <ctime>
 
 RobotomyRequestForm::RobotomyRequestForm()
     : AForm("RobotomyRequestForm", 72, 45),
@@ -36,17 +37,19 @@ const std::string &RobotomyRequestForm::getTarget() const
 
 void RobotomyRequestForm::execute(const Bureaucrat &executor) const
 {
+    std::srand(std::time(0));
     if (this->getSignal() == false)
         throw AForm::FormNotSignedException();
     
     if (executor.getGrade() > this->getGradeExecute())
         throw AForm::GradeTooLowException();
-        
+
     std::cout << "DRLLL DRLLLLL DRLLLZZZZZ" << std::endl;
 
-    if (rand() % 2)
+    if (std::rand() % 2)
         std::cout << this->getTarget() << " has been robotomized" << std::endl;
     else
         std::cout << this->getTarget() << " robotomy failed." << std::endl;
 
 }
+
