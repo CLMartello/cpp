660c9503d791c59d420aae59821450f2ba5f80f1
CLMartello <carolinamartello1@gmail.com>
2026-07-17 19:31:29 +0100

    create cpp08

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,30 @@
+#ifndef EASYFIND_HPP
+
+#define EASYFIND_HPP
+
+#include <exception>
+#include <iostream>
+#include <algorithm>
+
+//iterator is an object that behaves like a pointer
+template <typename T> void easyfind(T& container, int value)
+{
+    typename T::iterator it;
+    it = std::find(container.begin(), container.end(), value);
+    if (it != container.end())
+        std::cout << "Value " << value << " found!" << std::endl;
+    else
+        throw std::runtime_error("Value not found!\n");
+}
+
+template <typename T> void easyfind(const T& container, int value)
+{
+    typename T::const_iterator it;
+    it = std::find(container.begin(), container.end(), value);
+    if (it != container.end())
+        std::cout << "Value " << value << " found!" << std::endl;
+    else
+        throw std::runtime_error("Value not found!\n");
+}
+
+#endif
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,56 @@
+
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main()
+{
+    try
+    {        
+        std::vector<int> v;
+        for (int i = 0; i < 5; i++)
+            v.push_back(i);
+        std::cout << "Vector: ";
+        for (int i = 0; i < v.size(); i++)
+            std::cout << v[i] << " ";
+        std::cout << std::endl << std::endl;
+        std::cout << "Success:" << std::endl;
+        ::easyfind(v, 2);
+        std::cout << std::endl;
+        std::cout << "Fail:" << std::endl;
+        ::easyfind(v, 8);
+        std::cout << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << e.what();
+    }
+    std::cout << std::endl;
+    try
+    {
+        std::list<int> l;
+        for (int i = 2; i < 7; i++)
+        {
+            if (i % 2)
+                l.push_front(i);
+            else
+                l.push_back(i);
+        }
+        std::cout << "List: ";
+        for (auto it = l.begin(); it != l.end(); ++it)
+            std::cout << *it << " ";
+        std::cout << std::endl << std::endl;      
+        std::cout << "Success:" << std::endl;
+        ::easyfind(l, 2);
+        std::cout << std::endl;
+        std::cout << "Fail:" << std::endl;
+        ::easyfind(l, 8);
+        std::cout << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << e.what();
+    }
+    return (0);
+}
